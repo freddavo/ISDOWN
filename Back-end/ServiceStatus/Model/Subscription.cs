e48499d84589681607f12bf36643ffc5c6836da5
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-04-24 19:31:45 +0100

    Update (Subscription Class)

---
+++ Back-end/ServiceStatus/Model/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServiceStatus.Model
+{
+    [Table("subscription")]
+    public class Subscription
+    {
+        [Column("service_name")]
+        public Servico Service { get; set; }
+        [Column("person_id")]
+        public Person person { get; set; }
+        [Column("date")]
+        public DateTime Data { get; set; }
+    }
+}
