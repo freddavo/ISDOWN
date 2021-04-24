84322443d18115fbf6929241d787439eba339bf0
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-04-24 18:05:53 +0100

    Update (Tabela Person)

---
+++ Back-end/ServiceStatus/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServiceStatus.Model
+{
+    [Table("person")]
+    public class Person
+    {
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
---
+++ Back-end/ServiceStatus/db/migrations/V3__Create_Table_Person.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+)
