c039a9536e09422f9ce700aca09c0a59b64b742f
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-04-24 18:21:31 +0100

    Update (Historic.cs)

--- Back-end/ServiceStatus/Model/Historic.cs
+++ Back-end/ServiceStatus/Model/Historic.cs
@@ -6,17 +6,11 @@ namespace ServiceStatus.Model
     [Table("historic")]
     public class Historic
     {
-        [Column("id")]
-        public long Id { get; set; }
         [Column("date")]
         public DateTime Date { get; set; }
         [Column("service_name")]
         public Servico Service { get; set; }
         [Column("failure")]
         public Failure Failure { get; set; }
-        [Column("description")]
-        public string Description { get; set; }
-        [Column("status")]
-        public string Status { get; set; }
     }
 }
