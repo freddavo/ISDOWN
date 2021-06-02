24599254751dbb7c2ef5706132b347b5c79aeddf
josecarlos55 <46457623+josecarlos55@users.noreply.github.com>
2021-06-02 20:26:55 +0100

    Update CSS Tables
    Update CSS Tables

--- ServiceStatus/ServiceStatus/ClientApp/src/DNS/Dns.js
+++ ServiceStatus/ServiceStatus/ClientApp/src/DNS/Dns.js
@@ -5,6 +5,22 @@ import { NavLink } from 'react-router-dom';
 import { store } from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 import 'animate.css';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Avatar,
+    Grid,
+    Typography,
+    TablePagination,
+    TableFooter
+} from '@material-ui/core';
+
 
 
 export class Dns extends Component {
@@ -23,34 +39,101 @@ export class Dns extends Component {
         console.log(this.state.forecasts);
     }
 
-    
+
 
     render() {
 
         //let contents = this.renderForecastsTable(this.state.forecasts);
 
-        
+        const useStyles = makeStyles((theme) => ({
+            table: {
+                minWidth: 650,
+            },
+            tableContainer: {
+                borderRadius: 15,
+                margin: '10px 10px',
+                maxWidth: 950
+            },
+            tableHeaderCell: {
+                fontWeight: 'bold',
+                backgroundColor: theme.palette.primary.dark,
+                color: theme.palette.getContrastText(theme.palette.primary.dark)
+            },
+            avatar: {
+                backgroundColor: theme.palette.primary.light,
+                color: theme.palette.getContrastText(theme.palette.primary.light)
+            },
+            name: {
+                fontWeight: 'bold',
+                color: theme.palette.secondary.dark,
+                padding: '10px 10px'
+            },
+            status: {
+                fontWeight: 'bold',
+                fontSize: '0.75rem',
+                color: 'white',
+                backgroundColor: 'grey',
+                borderRadius: 8,
+                padding: '3px 10px',
+                display: 'inline-block'
+            }
+        }));
+
         return (
-            <div>
-                <table className='table table-striped variant= "dark"' >
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+
+            <TableContainer component={Paper} className={useStyles.tableContainer}
+
+                style={{
+                    borderRadius: 15,
+                    margin: '10px 10px',
+                    maxWidth: 1425
+                }}>
+                <Table className={useStyles.table}
+                    aria-label="simple table"
+                    style={{ minWidth: 650 }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className={useStyles.tableHeaderCell}
+                                style={{
+                                    fontWeight: 'bold'
+                                }} >Name</TableCell>
+                            <TableCell className={useStyles.tableHeaderCell}
+                                style={{
+                                    fontWeight: 'bold'
+                                }}>Status</TableCell>
+
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                         {this.state.forecasts.map(forecast => {
                             console.log(forecast);
                             return <tr key={forecast.name}>
-                                <td>{forecast.name}</td>
-                                <td>{forecast.status}</td>
-                                
+                                <TableCell>
+                                    <Typography>
+                                        {forecast.name}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell> <Typography className={useStyles.status}
+                                    style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '0.75rem',
+                                        color: 'white',
+                                        backgroundColor: 'grey',
+                                        borderRadius: 8,
+                                        padding: '3px 10px',
+                                        display: 'inline-block',
+                                        backgroundColor:
+                                            ((forecast.status === 'Success' && 'green') ||
+                                                (forecast.status === 'Uninitialized' && 'red'))
+                                    }}
+                                >{forecast.status}</Typography>
+                                </TableCell>
                             </tr>
                         })}
-                    </tbody>
-                </table>
-            </div>
+                    </TableBody>
+                </Table>
+            </ TableContainer>
+
         );
     }
 
--- ServiceStatus/ServiceStatus/ClientApp/src/components/Services.js
+++ ServiceStatus/ServiceStatus/ClientApp/src/components/Services.js
@@ -5,6 +5,24 @@ import { NavLink } from 'react-router-dom';
 import { store } from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 import 'animate.css';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Avatar,
+    Grid,
+    Typography,
+    TablePagination,
+    TableFooter
+} from '@material-ui/core';
+
+
+
 
 
 export class Services extends Component {
@@ -14,6 +32,7 @@ export class Services extends Component {
     constructor(props) {
         super(props);
         this.state = { forecasts: [], loading: true };
+     
 
         fetch('Service/Index')
             .then(response => response.json())
@@ -23,7 +42,8 @@ export class Services extends Component {
     }
 
      onSubmit () {
-       const history = useHistory();
+         const history = useHistory();
+         
        history.push('/Historic');
     }
 
@@ -41,38 +61,114 @@ export class Services extends Component {
             })
         }
 
+        const useStyles = makeStyles((theme) => ({
+            table: {
+                minWidth: 650,
+            },
+            tableContainer: {
+                borderRadius: 15,
+                margin: '10px 10px',
+                maxWidth: 950
+            },
+            tableHeaderCell: {
+                fontWeight: 'bold',
+                backgroundColor: theme.palette.primary.dark,
+                color: theme.palette.getContrastText(theme.palette.primary.dark)
+            },
+            avatar: {
+                backgroundColor: theme.palette.primary.light,
+                color: theme.palette.getContrastText(theme.palette.primary.light)
+            },
+            name: {
+                fontWeight: 'bold',
+                color: theme.palette.secondary.dark,
+                padding: '10px 10px'
+            },
+            status: {
+                fontWeight: 'bold',
+                fontSize: '0.75rem',
+                color: 'white',
+                backgroundColor: 'grey',
+                borderRadius: 8,
+                padding: '3px 10px',
+                display: 'inline-block'
+            }
+        }));
+
+        
+
         return (
-            <div>
-                <table className= 'table table-striped variant= "dark"' >
-                    <thead>
-                        <tr> 
-                            <th>Name</th>
-                            <th>Health State</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+           
+            <TableContainer component={Paper} className={useStyles.tableContainer}
+
+                style={{
+                borderRadius: 15,
+                    margin: '10px 10px',
+                    maxWidth: 1425
+                }}> 
+                <Table className={useStyles.table}
+                    aria-label="simple table"
+                    style={{ minWidth: 650 }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className={useStyles.tableHeaderCell} style={{
+                                fontWeight: 'bold'}} >Services</TableCell>
+                            <TableCell className={useStyles.tableHeaderCell} style={{
+                                fontWeight: 'bold'
+                            }}>Path</TableCell>
+                            <TableCell className={useStyles.tableHeaderCell} style={{
+                                fontWeight: 'bold'
+                            }}>Status</TableCell>
+                            <TableCell className={useStyles.tableHeaderCell} style={{
+                                fontWeight: 'bold'
+                            }}></TableCell>
+                            <TableCell className={useStyles.tableHeaderCell} style={{
+                                fontWeight: 'bold'
+                            }}></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                         {this.state.forecasts.map(forecast => {
                             console.log(forecast);
                             return <tr key={forecast.name}>
-                                <td>{forecast.name}</td>
-                                <td>{forecast.health_State}</td>
-                                <td>{forecast.path}</td>
-                                <td><NavLink exact activeClassName="active"
+                                <TableCell>
+                                    <Typography>
+                                        {forecast.name}
+                                    </Typography>
+                                </TableCell>
+                                
+                                <TableCell> <Typography>{forecast.path}</Typography></TableCell>
+                                <TableCell> <Typography className={useStyles.status}
+                                    style={{
+                                        fontWeight: 'bold',
+                                        fontSize: '0.75rem',
+                                        color: 'white',
+                                        backgroundColor: 'grey',
+                                        borderRadius: 8,
+                                        padding: '3px 10px',
+                                        display: 'inline-block',
+                                        backgroundColor:
+                                            ((forecast.health_State === 'Success' && 'green') ||
+                                                (forecast.health_State === 'Error' && 'red'))
+                                    }}
+                                >{forecast.health_State}</Typography>
+                                </TableCell>
+                                <TableCell> <Typography><NavLink exact activeClassName="active"
                                     to={{
                                         pathname: "/Historic",
                                         props: {
                                             state: [
-                                            forecast.name]
+                                                forecast.name]
                                         }
-                                    }}> Historic </NavLink></td> 
-                                <td><button onClick={handleOnClickDefault}>
-                                    Subscribe </button> </td>
+                                    }}> Historic </NavLink></Typography></TableCell>
+                                <TableCell> <Typography><button onClick={handleOnClickDefault}>
+                                    Subscribe </button> </Typography></TableCell>
                             </tr>
                         })}
-                    </tbody>
-                </table>
-            </div>  
+                    </TableBody>
+                </Table>
+                    
+            </ TableContainer>
         );
     }
 
